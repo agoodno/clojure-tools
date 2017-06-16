e8b86ee20f28c4f4717e9613b342d42d86a621e4
Andrew Goodnough <agoodnough@primexinc.com>
2017-06-16 14:57:16 -0500

    Peek displays account ids not found in database

--- src/scripts/peek_at_failed_account_alert_settings.clj
+++ src/scripts/peek_at_failed_account_alert_settings.clj
@@ -1,10 +1,11 @@
 (ns scripts.peek-at-failed-account-alert-settings
-  (:require [tools.primex.aws :as paws]))
+  (:require [tools.primex.aws :as paws]
+            [tools.primex.customer-service :as cs]))
 
 (defn -main
   [& args]
   (let [[prefix environment queue-name] args
         account-ids (atom #{})]
-    (do
-      (paws/peek-and-save-failed-account-ids prefix environment queue-name account-ids)
-      (println @account-ids))))
+    (dotimes [n 10]
+      (paws/peek-and-save-failed-account-ids prefix environment queue-name account-ids))
+    (println (cs/list-accts-not-found environment @account-ids))))
--- src/tools/primex/customer_service.clj
+++ src/tools/primex/customer_service.clj
@@ -1,6 +1,8 @@
 (ns tools.primex.customer-service
   (:require [tools.db :as db]
-            [clojure.java.jdbc :as jdbc]))
+            [clojure.java.jdbc :as jdbc]
+            [clojure.set :as set]
+            [clojure.test :as test]))
 
 (def dev-db {:dbtype "postgresql"
              :dbname "customerservice_development"
@@ -11,3 +13,13 @@
 (defn list-acct-basic-info [environment]
   (let [accts (jdbc/query dev-db ["SELECT * FROM \"accounts\""])]
     (map #(select-keys % [:id :name]) accts)))
+
+(defn list-accts-not-found [environment unique_acct_ids]
+  "Compares a set of unique account ids to what is in the database and
+  prints out account ids that are missing in the database."
+  (let [accts_delim (apply str (interpose \, unique_acct_ids))
+        db_acct_ids (map :id (jdbc/query dev-db [(str
+                                                  "SELECT id FROM accounts WHERE id IN ("
+                                                  accts_delim
+                                                  ")")]))]
+    (set/difference unique_acct_ids (set/intersection unique_acct_ids (set db_acct_ids)))))
